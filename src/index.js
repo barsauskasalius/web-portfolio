dc833b23ab658e4be72a3c646c7280454b59db49
Alius Barsauskas <78370707+barsauskasalius@users.noreply.github.com>
2021-10-02 16:55:28 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  Date...
+const display = document.querySelector('.about-me')
+
+const today = new Date()
+const date = today.getDate()
+const day = today.getDay()
+const hours = today.getHours()
+const isoString = today.toISOString()
+console.log(isoString)
+
+
+const Christmas = new Date("2021-12-24T10:17:24.546Z")
+
+const millisecondsToXmas = Christmas - today
+const minutes = (millisecondsToXmas / 60000).toString()
+console.log(minutes)
+display.innerHTML = Math.round(minutes)
+
+*/
